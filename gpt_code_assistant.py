717441f247bff2faa9af3cce9729a712f62f917c
Ericson Willians <ericson.willians@tech4h.com.br>
2023-04-29 16:19:46 -0300

    feat: added .gitignore file exclusion and improved output

--- gpt_code_assistant.py
+++ gpt_code_assistant.py
@@ -2,14 +2,18 @@ import os
 import sys
 import json
 import openai
+import pathspec
 from pathlib import Path
 import tiktoken
 
 openai.api_key = os.environ["OPEN_AI_API_KEY"]
 
 
-def read_supported_files(directory, extensions):
+def read_supported_files(directory, extensions, gitignore_spec=None):
     for entry in directory.iterdir():
+        if gitignore_spec and gitignore_spec.match_file(entry):
+            continue
+
         if entry.is_file() and entry.suffix in extensions:
             try:
                 with open(entry, "r", encoding="utf-8", errors="ignore") as f:
@@ -17,7 +21,17 @@ def read_supported_files(directory, extensions):
             except UnicodeDecodeError:
                 print(f"Warning: Ignoring file with encoding issues: {entry}")
         elif entry.is_dir():
-            yield from read_supported_files(entry, extensions)
+            yield from read_supported_files(entry, extensions, gitignore_spec)
+
+
+def load_gitignore_patterns(directory):
+    gitignore_path = directory / ".gitignore"
+    if gitignore_path.exists():
+        with open(gitignore_path, "r") as f:
+            gitignore_content = f.read()
+        return pathspec.PathSpec.from_lines("gitwildmatch", gitignore_content.splitlines())
+    else:
+        return None
 
 
 def count_tokens(string: str) -> int:
@@ -38,8 +52,9 @@ def main():
         supported_extensions_data = json.load(f)
         supported_extensions = set(supported_extensions_data["extensions"])
 
+    gitignore_spec = load_gitignore_patterns(codebase_directory)
     codebase_context = "\n".join(read_supported_files(
-        codebase_directory, supported_extensions))
+        codebase_directory, supported_extensions, gitignore_spec))
     token_count = count_tokens(codebase_context)
     cost_per_token = 0.002 / 1000
     estimated_cost = token_count * cost_per_token
@@ -53,16 +68,30 @@ def main():
         prompt = input("Enter your prompt: ")
         model_engine = "gpt-3.5-turbo"
 
-        response = openai.Completion.create(
-            engine=model_engine,
-            prompt=codebase_context + "\n" + prompt,
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.8,
-        )
+        # Split the input message into parts of length 4096 tokens
+        message_parts = [codebase_context[i:i+4096]
+                         for i in range(0, len(codebase_context), 4096)]
 
-        print(response.choices[0].text.strip())
+        # Make multiple API calls to provide the full data
+        response_parts = []
+        for part in message_parts:
+            response = openai.ChatCompletion.create(
+                model=model_engine,
+                messages=[
+                    {"role": "user", "content": part + "\n" + prompt}
+                ]
+            )
+            if len(response.choices) > 0 and hasattr(response.choices[0], 'message'):
+                response_parts.append(response.choices[0].message)
+
+        # Combine the response parts into a single response
+        full_response = ""
+        i = 1
+        for response in response_parts:
+            full_response += str(i) + '.: ' + response.content + '\n\n'
+            i += 1
+
+        print(full_response.strip())
     else:
         print("Operation aborted.")
 
